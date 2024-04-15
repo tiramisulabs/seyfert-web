745ad0ed85d27590ed9172c1d8aacccd2dff1f9c
FabrizioCoder <fabrizioivan36@gmail.com>
2024-04-14 20:24:06 -0600

    Fix font import and update metadata in layout.tsx

--- src/app/benchmarks/page.tsx
+++ src/app/benchmarks/page.tsx
@@ -1,12 +1,12 @@
-"use client"
+'use client';
 
-import AnimatedText from "react-animated-text-content"
-import { Line } from "react-chartjs-2"
-import detritusData from "../../../public/data/detritus-client-98045.json"
-import discordjsData from "../../../public/data/discord_js-98045.json"
-import erisData from "../../../public/data/eris-98045.json"
-import oceanicData from "../../../public/data/oceanic_js-98045.json"
-import seyfertData from "../../../public/data/seyfert-98045.json"
+import AnimatedText from 'react-animated-text-content';
+import { Line } from 'react-chartjs-2';
+import detritusData from '../../../public/data/detritus-client-98045.json';
+import discordjsData from '../../../public/data/discord_js-98045.json';
+import erisData from '../../../public/data/eris-98045.json';
+import oceanicData from '../../../public/data/oceanic_js-98045.json';
+import seyfertData from '../../../public/data/seyfert-98045.json';
 
 import {
   CategoryScale,
@@ -17,8 +17,8 @@ import {
   PointElement,
   Title,
   Tooltip,
-} from "chart.js"
-import { motion } from "framer-motion"
+} from 'chart.js';
+import { motion } from 'framer-motion';
 
 ChartJS.register(
   CategoryScale,
@@ -32,88 +32,93 @@ ChartJS.register(
 
 const totalDuration = 2e3;
 const delayBetweenPoints = totalDuration / seyfertData.length;
-const previousY = (ctx:any) => ctx.index === 0 ? ctx.chart.scales.y.getPixelForValue(100) : ctx.chart.getDatasetMeta(ctx.datasetIndex).data[ctx.index - 1].getProps(['y'], true).y;
+const previousY = (ctx: any) =>
+  ctx.index === 0
+    ? ctx.chart.scales.y.getPixelForValue(100)
+    : ctx.chart
+        .getDatasetMeta(ctx.datasetIndex)
+        .data[ctx.index - 1].getProps(['y'], true).y;
 const animation = {
   x: {
     type: 'number',
     easing: 'linear',
     duration: delayBetweenPoints,
     from: NaN, // the point is initially skipped
-    delay(ctx:any) {
+    delay(ctx: any) {
       if (ctx.type !== 'data' || ctx.xStarted) {
         return 0;
       }
       ctx.xStarted = true;
       return ctx.index * delayBetweenPoints;
-    }
+    },
   },
   y: {
     type: 'number',
     easing: 'linear',
     duration: delayBetweenPoints,
     from: previousY,
-    delay(ctx:any) {
+    delay(ctx: any) {
       if (ctx.type !== 'data' || ctx.yStarted) {
         return 0;
       }
       ctx.yStarted = true;
       return ctx.index * delayBetweenPoints;
-    }
-  }
+    },
+  },
 };
 
 const data = {
   labels: seyfertData.map((_, i) => `${i} minutes`),
   datasets: [
     {
-      label: "Seyfert",
+      label: 'Seyfert',
       data: seyfertData.map((_) => _.rss / 1024 / 1024),
       fill: false,
-      borderColor: "rgb(75, 192, 192)",
+      borderColor: 'rgb(75, 192, 192)',
       tension: 0.1,
     },
     {
-      label: "Discord.js",
+      label: 'Discord.js',
       data: discordjsData.map((_) => _.rss / 1024 / 1024),
       fill: false,
-      borderColor: "rgb(88, 101, 242)",
+      borderColor: 'rgb(88, 101, 242)',
       tension: 0.1,
     },
     {
-      label: "Oceanic.js",
+      label: 'Oceanic.js',
       data: oceanicData.map((_) => _.rss / 1024 / 1024),
       fill: false,
-      borderColor: "rgb(153, 102, 255)",
+      borderColor: 'rgb(153, 102, 255)',
       tension: 0.1,
     },
     {
-      label: "Eris",
+      label: 'Eris',
       data: erisData.map((_) => _.rss / 1024 / 1024),
       fill: false,
-      borderColor: "rgb(255, 99, 132)",
+      borderColor: 'rgb(255, 99, 132)',
       tension: 0.1,
     },
     {
-      label: "Detritus",
+      label: 'Detritus',
       data: detritusData.map((_) => _.rss / 1024 / 1024),
       fill: false,
-      borderColor: "rgb(255, 183, 79)",
+      borderColor: 'rgb(255, 183, 79)',
       tension: 0.1,
-    }
+    },
   ],
-}
+};
 
 export default function Benchmark() {
   return (
     <>
-      <div className='flex justify-between items-start gap-14'>
-        <div className='flex flex-col gap-2'>
+      <div className="flex justify-between items-start gap-14">
+        <div className="flex flex-col gap-2">
           <AnimatedText
             type="chars"
             animation={{
               scale: 1.1,
             }}
-            animationType={"lights"}
+            animationType={'lights'}
             interval={0.04}
             duration={0.8}
             tag="h1"
@@ -124,33 +129,49 @@ export default function Benchmark() {
           >
             Benchmarks
           </AnimatedText>
-          <motion.p className='text-default-500 duration-200 lg:text-base text-sm' initial={{ y: 10, opacity: 0 }}
+          <motion.p
+            className="text-default-500 duration-200 lg:text-base text-sm"
+            initial={{ y: 10, opacity: 0 }}
             animate={{ y: 0, opacity: 100 }}
             transition={{
               duration: 0.5,
-              delay: 0.5
-            }}>
-            As you can see below, Seyfert is the most efficient in terms of memory usage at long runtime. All libraries were run on a Discord bot within 3,000 servers and with all non-privileged intents enabled, you can see the code used to benchmark the libraries <a href="https://github.com/tiramisulabs/benchmark" className="font-bold text-indigo-600">here</a>.
+              delay: 0.5,
+            }}
+          >
+            As you can see below, Seyfert is the most efficient in terms of
+            memory usage at long runtime. All libraries were run on a Discord
+            bot within 3,000 servers and with all non-privileged intents
+            enabled, you can see the code used to benchmark the libraries{' '}
+            <a
+              href="https://github.com/tiramisulabs/benchmark"
+              className="font-bold text-indigo-600"
+            >
+              here
+            </a>
+            .
           </motion.p>
         </div>
-
       </div>
       <div className="max-h-[26em]">
-        <Line className="h-[26em]" data={data} options={{
-          //@ts-expect-error
-          animation,
-          maintainAspectRatio: false,
-          scales: {
-            y: {
-              ticks: {
-                callback: function (value) {
-                  return `${value}MB`
-                }
-              }
-            }
-          }
-        }} />
+        <Line
+          className="h-[26em]"
+          data={data}
+          options={{
+            //@ts-expect-error
+            animation,
+            maintainAspectRatio: false,
+            scales: {
+              y: {
+                ticks: {
+                  callback: function (value) {
+                    return `${value}MB`;
+                  },
+                },
+              },
+            },
+          }}
+        />
       </div>
     </>
-  )
+  );
 }
--- src/app/credits/page.tsx
+++ src/app/credits/page.tsx
@@ -1,122 +1,177 @@
-"use client";
+'use client';
 
 import AnimatedText from 'react-animated-text-content';
-import { motion } from "framer-motion"
+import { motion } from 'framer-motion';
 import { HeartIcon } from '@heroicons/react/20/solid';
 import Link from 'next/link';
 
 const credits = {
-    main: [
-        {
-            social: "https://github.com/marcrock22",
-            name: "Marcrock22"
-        },
-        {
-            social: "https://github.com/socram09",
-            name: "Socram09"
-        },
-        {
-            social: "https://github.com/freeaoi",
-            name: "FreeAoi"
-        }
-    ],
-    web: [
-        {
-            social: "https://github.com/simxnet",
-            name: "Simxnet",
-            quote: "Me, lol"
-        }
-    ],
-    extra: [
-        {
-            social: "https://github.com/yuzudev",
-            name: "Yuzu",
-            quote: "Biscuit.js founder"
-        },
-        {
-            social: "https://github.com/miiadev",
-            name: "Miia",
-            quote: "Domain registration, consuntalcy, etc"
-        },
-        {
-            social: "https://github.com/drylozu",
-            name: "David",
-            quote: "Cat officer 🐈"
-        }
-    ]
-}
+  main: [
+    {
+      social: 'https://github.com/marcrock22',
+      name: 'Marcrock22',
+    },
+    {
+      social: 'https://github.com/socram09',
+      name: 'Socram09',
+    },
+    {
+      social: 'https://github.com/freeaoi',
+      name: 'FreeAoi',
+    },
+  ],
+  web: [
+    {
+      social: 'https://github.com/simxnet',
+      name: 'Simxnet',
+      quote: 'Me, lol',
+    },
+  ],
+  docs: [
+    {
+      social: 'https://github.com/FabrizioCoder',
+      name: 'Fabrizio Santana',
+      quote: 'falling in love with a... biscuit? 🍪',
+    },
+    {
+      social: 'https://github.com/PabloRNC',
+      name: 'PabloRNC',
+      quote: 'cool guy 😎',
+    },
+  ],
+  extra: [
+    {
+      social: 'https://github.com/yuzudev',
+      name: 'Yuzu',
+      quote: 'Biscuit.js founder',
+    },
+    {
+      social: 'https://github.com/miiadev',
+      name: 'Miia',
+      quote: 'Domain registration, consuntalcy, etc',
+    },
+    {
+      social: 'https://github.com/drylozu',
+      name: 'David',
+      quote: 'Cat officer 🐈',
+    },
+  ],
+};
 
 export default function Page() {
-    return <div className=''>
-        <div className='flex justify-between items-start gap-14'>
-            <div className='flex flex-col gap-2'>
-                <AnimatedText
-                    type="chars"
-                    animation={{
-                        scale: 1.1,
-                    }}
-                    animationType={"lights"}
-                    interval={0.04}
-                    duration={0.8}
-                    tag="h1"
-                    className="lg:text-5xl text-4xl font-black duration-150"
-                    includeWhiteSpaces
-                    threshold={0.1}
-                    rootMargin="20%"
-                >
-                    Credits
-                </AnimatedText>
-                <motion.p className='text-default-500 duration-200 flex items-center gap-1 lg:text-base text-sm' initial={{ y: 10, opacity: 0 }}
-                    animate={{ y: 0, opacity: 100 }}
-                    transition={{
-                        duration: 0.5,
-                        delay: 0.5
-                    }}>
-                    Thanks to these amazing people, Seyfert is possible <HeartIcon className='w-5 h-5 text-rose-500' />
-                </motion.p>
-                <motion.p className='text-default-500 duration-200 gap-1 lg:text-base text-sm' initial={{ y: 10, opacity: 0 }}
-                    animate={{ y: 0, opacity: 100 }}
-                    transition={{
-                        duration: 0.5,
-                        delay: 0.6
-                    }}>
-                    Hey, this is a message from Simxnet, the creator of this website. I just wanted to say that I also made a Discord botlist if you want to check it out you know...
-                    <Link className='underline ml-1 text-default-800 hover:text-default-700' target="_blank" referrerPolicy='origin' href="https://dbots.fun?ref=seyfert.dev">dbots.fun</Link>
-                </motion.p>
-            </div>
-        </div>
-        <motion.div className='my-5 flex flex-col gap-4' initial={{ y: 10, opacity: 0 }}
+  return (
+    <div className="">
+      <div className="flex justify-between items-start gap-14">
+        <div className="flex flex-col gap-2">
+          <AnimatedText
+            type="chars"
+            animation={{
+              scale: 1.1,
+            }}
+            animationType={'lights'}
+            interval={0.04}
+            duration={0.8}
+            tag="h1"
+            className="lg:text-5xl text-4xl font-black duration-150"
+            includeWhiteSpaces
+            threshold={0.1}
+            rootMargin="20%"
+          >
+            Credits
+          </AnimatedText>
+          <motion.p
+            className="text-default-500 duration-200 flex items-center gap-1 lg:text-base text-sm"
+            initial={{ y: 10, opacity: 0 }}
             animate={{ y: 0, opacity: 100 }}
             transition={{
-                duration: 0.5,
-                delay: 1
-            }}>
-            <div className='flex flex-col gap-2'>
-                <h3 className='font-bold text-2xl'>Main development</h3>
-                <div className='flex flex-col gap-1 w-fit'>
-                    {credits.main.map(u => <Link href={u.social} key={u.name}>
-                        <div className="relative before:absolute before:bg-primary before:bottom-0 before:left-0 before:h-full before:w-full before:origin-bottom before:scale-y-[0.35] hover:before:scale-y-100 before:transition-transform before:ease-in-out before:duration-500">
-                            <span className="relative">{u.name}</span>
-                        </div>
-                    </Link>)}
+              duration: 0.5,
+              delay: 0.5,
+            }}
+          >
+            Thanks to these amazing people, Seyfert is possible{' '}
+            <HeartIcon className="w-5 h-5 text-rose-500" />
+          </motion.p>
+          <motion.p
+            className="text-default-500 duration-200 gap-1 lg:text-base text-sm"
+            initial={{ y: 10, opacity: 0 }}
+            animate={{ y: 0, opacity: 100 }}
+            transition={{
+              duration: 0.5,
+              delay: 0.6,
+            }}
+          >
+            Hey, this is a message from Simxnet, the creator of this website. I
+            just wanted to say that I also made a Discord botlist if you want to
+            check it out you know...
+            <Link
+              className="underline ml-1 text-default-800 hover:text-default-700"
+              target="_blank"
+              referrerPolicy="origin"
+              href="https://dbots.fun?ref=seyfert.dev"
+            >
+              dbots.fun
+            </Link>
+          </motion.p>
+        </div>
+      </div>
+      <motion.div
+        className="my-5 flex flex-col gap-4"
+        initial={{ y: 10, opacity: 0 }}
+        animate={{ y: 0, opacity: 100 }}
+        transition={{
+          duration: 0.5,
+          delay: 1,
+        }}
+      >
+        <div className="flex flex-col gap-2">
+          <h3 className="font-bold text-2xl">Main development</h3>
+          <div className="flex flex-col gap-1 w-fit">
+            {credits.main.map((u) => (
+              <Link href={u.social} key={u.name}>
+                <div className="relative before:absolute before:bg-primary before:bottom-0 before:left-0 before:h-full before:w-full before:origin-bottom before:scale-y-[0.35] hover:before:scale-y-100 before:transition-transform before:ease-in-out before:duration-500">
+                  <span className="relative">{u.name}</span>
                 </div>
-            </div>
-            <div className='flex flex-col gap-2 opacity-70'>
-                <h3 className='font-bold text-2xl'>Web development</h3>
-                <div className='flex flex-col gap-1'>
-                    {credits.web.map(u => <Link href={u.social} key={u.name}>
-                        <span>{u.name} <span className='text-default-500'>— {u.quote}</span></span>
-                    </Link>)}
-                </div>
-            </div>
-            <div className='flex flex-col gap-2 opacity-50'>
-                <h3 className='font-bold text-2xl'>Extras</h3>
-                <div className='flex flex-col gap-1'>
-                    {credits.extra.map(u => <Link href={u.social} key={u.name}>
-                        <span>{u.name} <span className='text-default-500'>— {u.quote}</span></span>
-                    </Link>)}
-                </div>
-            </div>
-        </motion.div>
+              </Link>
+            ))}
+          </div>
+        </div>
+        <div className="flex flex-col gap-2 opacity-70">
+          <h3 className="font-bold text-2xl">Web development</h3>
+          <div className="flex flex-col gap-1">
+            {credits.web.map((u) => (
+              <Link href={u.social} key={u.name}>
+                <span>
+                  {u.name} <span className="text-default-500">— {u.quote}</span>
+                </span>
+              </Link>
+            ))}
+          </div>
+        </div>
+        <div className="flex flex-col gap-2 opacity-70">
+          <h3 className="font-bold text-2xl">Docs development</h3>
+          <div className="flex flex-col gap-1">
+            {credits.docs.map((u) => (
+              <Link href={u.social} key={u.name}>
+                <span>
+                  {u.name} <span className="text-default-500">— {u.quote}</span>
+                </span>
+              </Link>
+            ))}
+          </div>
+        </div>
+        <div className="flex flex-col gap-2 opacity-50">
+          <h3 className="font-bold text-2xl">Extras</h3>
+          <div className="flex flex-col gap-1">
+            {credits.extra.map((u) => (
+              <Link href={u.social} key={u.name}>
+                <span>
+                  {u.name} <span className="text-default-500">— {u.quote}</span>
+                </span>
+              </Link>
+            ))}
+          </div>
+        </div>
+      </motion.div>
     </div>
+  );
 }
