5284449201b258824ba56a4af4f63607ae6f81b8
Drylozu <35281350+Drylozu@users.noreply.github.com>
2024-12-22 20:08:26 -0500

    feat: update images

--- src/components/modules/home/haters.tsx
+++ src/components/modules/home/haters.tsx
@@ -9,56 +9,47 @@ import Image from "next/image";
 
 const haters: {
 	displayName: string;
-	username: string;
-	avatar: string;
+	username: string
 	content: React.ReactNode;
 }[] = [
 		{
 			displayName: "Socram09",
 			username: "socram09",
-			avatar: "d038d272c589230c38f84243789692c3",
 			content: <>I have no idea how this thing turns on.</>,
 		},
 		{
 			displayName: "MARCROCK22",
 			username: "marcrock22",
-			avatar: "6aff65edc136278714c4d4998dca4680",
 			content: <>I don't even know why I made this shit</>
 		},
 		{
 			displayName: "FreeAoi",
 			username: "freeaoi",
-			avatar: "324f47e4cdb102f41303d7edb0213175",
 			content: <>GOMEN AMANAI</>
 		},
 		{
 			displayName: "Deivid",
 			username: "deivid",
-			avatar: "a_842b80d2226ed205deeec10cd2a99ca9",
 			content: <>Breakfast every day. Today I am fasting.</>
 		},
 		{
 			displayName: "JustEvil",
 			username: "justevil",
-			avatar: "0f7fcc364251c08e705dfffed8182091",
 			content: <>Is so good that I can't even use it.</>
 		},
 		{
 			displayName: "Sawako",
 			username: "sawa_ko",
-			avatar: "3cfd56ea7764ac48aa1b33874edc8d64",
 			content: <>I hate seyfert so much that I sponsor them &gt;:(</>
 		},
 		{
 			displayName: "Miia",
 			username: "miia",
-			avatar: "ae86fffd53d44c4c2f9cf532e18d4e61",
 			content: <>Seyfert works until you switch it on.</>
 		},
 		{
 			displayName: "Shisui San",
 			username: "shisuisan",
-			avatar: "a_b490bbd37a4037949d0868c222fa7033",
 			content: "Seyfert better keep working else I will be fired"
 		}
 	];
@@ -86,7 +77,7 @@ export default function HomeHaters() {
 										width={50}
 										height={50}
 										alt={hater.displayName}
-										src={`/haters/${hater.avatar}.png`}
+										src={`/haters/${hater.username}.png`}
 										className={css({
 											rounded: "full",
 										})}
